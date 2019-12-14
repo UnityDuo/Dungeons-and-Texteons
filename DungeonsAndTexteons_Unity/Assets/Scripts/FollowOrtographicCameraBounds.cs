7bb060e42d9adb8db42d5cd16827b22a55d17864
Gianni Vallerini <gianni.vallerini@hotmail.com>
2019-12-14 20:48:07 +0100

    [TECH] Added WASD input for player controller

---
+++ DungeonsAndTexteons_Unity/Assets/Scripts/FollowOrtographicCameraBounds.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonsAndTexteons
+{
+
+    public class FollowOrtographicCameraBounds : MonoBehaviour
+    {
+        #region Fields
+
+        public enum CameraBound
+        {
+            Front,
+            Rear,
+            Left,
+            Right,
+        }
+
+        public BoxCollider2D collToControl;
+        public CameraBound sideToFollow;
+        public Camera ortoCamera;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void FixedUpdate()
+        {
+            ReplaceCollider();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void ReplaceCollider()
+        {
+            float offsetMultiplier = 0;
+            float sizeMultiplier = 0;
+            switch (sideToFollow)
+            {
+                case CameraBound.Front:
+                    offsetMultiplier = 1f;
+                    sizeMultiplier = 1.45f;
+                    collToControl.offset = new Vector2(0, offsetMultiplier * ortoCamera.orthographicSize);
+                    collToControl.size = new Vector2(sizeMultiplier * ortoCamera.orthographicSize, 1);
+                    break;
+                case CameraBound.Rear:
+                    offsetMultiplier = -1f;
+                    sizeMultiplier = 1.45f;
+                    collToControl.offset = new Vector2(0, offsetMultiplier * ortoCamera.orthographicSize);
+                    collToControl.size = new Vector2(sizeMultiplier * ortoCamera.orthographicSize, 1);
+                    break;
+                case CameraBound.Right:
+                    offsetMultiplier = .62f;
+                    sizeMultiplier = 2f;
+                    collToControl.offset = new Vector2(offsetMultiplier * ortoCamera.orthographicSize, 0);
+                    collToControl.size = new Vector2(1, sizeMultiplier * ortoCamera.orthographicSize);
+                    break;
+                case CameraBound.Left:
+                    offsetMultiplier = -.62f;
+                    sizeMultiplier = 2;
+                    collToControl.offset = new Vector2(offsetMultiplier * ortoCamera.orthographicSize, 0);
+                    collToControl.size = new Vector2(1, sizeMultiplier * ortoCamera.orthographicSize);
+                    break;
+            }
+        }
+
+        #endregion
+    }
+
+}
---
+++ DungeonsAndTexteons_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DungeonsAndTexteons
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        #region Fields
+
+        public float maxSpeed = 5f;
+        public float acceleration = 1f;
+        public float deceleration = .5f;
+
+        [SerializeField] private KeyCode moveForward = KeyCode.W;
+        [SerializeField] private KeyCode moveBackwards = KeyCode.S;
+        [SerializeField] private KeyCode moveRight = KeyCode.D;
+        [SerializeField] private KeyCode moveLeft = KeyCode.A;
+
+        private float vInput = 0;
+        private float hInput = 0;
+        private float vSpeed = 0;
+        private float hSpeed = 0;
+        private Vector2 speed = Vector2.zero;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Update()
+        {
+            CheckInputs();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void CheckInputs()
+        {
+            //Get Inputs
+            vInput = 0;
+            hInput = 0;
+
+            if (Input.GetKey(moveForward))
+            {
+                vInput++;
+            }
+            if (Input.GetKey(moveBackwards))
+            {
+                vInput--;
+            }
+            if (Input.GetKey(moveRight))
+            {
+                hInput++;
+            }
+            if (Input.GetKey(moveLeft))
+            {
+                hInput--;
+            }
+
+            //Accelerate
+            vSpeed += vInput * acceleration * Time.deltaTime;
+            hSpeed += hInput * acceleration * Time.deltaTime;
+
+            //Decelerate
+            if (vInput == 0 && vSpeed != 0)
+            {
+                vSpeed -= Mathf.Sign(vSpeed) * deceleration * Time.deltaTime;
+            }
+            if (hInput == 0 && hSpeed != 0)
+            {
+                hSpeed -= Mathf.Sign(hSpeed) * deceleration * Time.deltaTime;
+            }
+
+            //Snap speed to Zero
+            if(vInput == 0 && vSpeed > -deceleration * Time.deltaTime && vSpeed < deceleration * Time.deltaTime)
+            {
+                vSpeed = 0;
+            }
+            if(hInput == 0 && hSpeed > -deceleration * Time.deltaTime && hSpeed < deceleration * Time.deltaTime)
+            {
+                hSpeed = 0;
+            }
+
+            //Clamp Speed
+            speed = new Vector2(hSpeed, vSpeed);
+
+            if (speed.magnitude > maxSpeed * Time.deltaTime)
+            {
+                speed = speed.normalized * maxSpeed * Time.deltaTime;
+                vSpeed = speed.y;
+                hSpeed = speed.x;
+            }
+        }
+
+        private void Move()
+        {
+            transform.position += (Vector3)speed;
+        }
+
+        #endregion
+    }
+
+}
